fca4550bb36bbbbcca7097f139739c2223e09ab5
Izac Lorimer <izac@gordhoard.org>
2020-04-24 23:28:54 -0800

    Make things actually work well

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { CommandClient, Message, GuildChannel } from 'eris'
 import dd from 'datadog-metrics'
+import emoji from 'node-emoji'
 
 const statusUpdate = () => {
   if (!(bot as any).ready) {
@@ -28,10 +29,10 @@ const statusUpdate = () => {
       }
       guildStatus[member.status as 'online' | 'idle' | 'dnd' | 'offline']++
     })
-    dd.gauge(`members_${guild.id}.online`, guildStatus.online)
-    dd.gauge(`members_${guild.id}.idle`, guildStatus.idle)
-    dd.gauge(`members_${guild.id}.dnd`, guildStatus.dnd)
-    dd.gauge(`members_${guild.id}.offline`, guildStatus.offline)
+    dd.gauge(`members.online`, guildStatus.online, [ `guild:${guild.id}` ])
+    dd.gauge(`members.idle`, guildStatus.idle, [ `guild:${guild.id}` ])
+    dd.gauge(`members.dnd`, guildStatus.dnd, [ `guild:${guild.id}` ])
+    dd.gauge(`members.offline`, guildStatus.offline, [ `guild:${guild.id}` ])
   })
 
   dd.gauge('users.online', status.online)
@@ -42,63 +43,140 @@ const statusUpdate = () => {
 
 const bot = new CommandClient(process.env.TOKEN)
 dd.init({
-  prefix: 'dev.bots.nookstats.',
-  apiKey: process.env.DATADOG_KEY
+  prefix: process.env.DATADOG_PREFIX,
+  apiKey: process.env.DATADOG_KEY,
+  flushIntervalSeconds: 0
 })
 
+setInterval(() => {
+  dd.flush(() => {
+    console.log('[INFO] Flushed to Datadog')
+  }, err => {
+    console.error(`[ERROR] Unable to flush to Datadog. ${err}`)
+  })
+}, 15000)
+
 bot.on('messageCreate', msg => {
-  dd.increment(`message.creates`)
-  dd.increment(`message.creates_${msg.channel.id}`)
-  switch (msg.channel.type) {
-    case 0:
-      dd.increment(`guild-message.creates_${(msg.channel as GuildChannel).guild.id}`)
-      break
-    case 1:
-      dd.increment(`dm-message.creates`)
-      break
+  console.log(`[INFO] Created ${msg.id}`)
+  const tags = [
+    `channel:${msg.channel.id}`,
+    `user:${msg.author.id}`,
+    `msg:${msg.id}`,
+    `chan-type:${msg.channel.type}`
+  ]
+  if ((msg.channel as GuildChannel).guild !== undefined) {
+    tags.push(`guild:${(msg.channel as GuildChannel).guild.id}`)
   }
+  dd.increment(`message.creates`, 1, tags)
 })
 
 bot.on('messageUpdate', msg => {
-  dd.increment(`message.edits`)
-  dd.increment(`message.edits_${msg.channel.id}`)
-  switch (msg.channel.type) {
-    case 0:
-      dd.increment(`guild-message.edits_${(msg.channel as GuildChannel).guild.id}`)
-      break
-    case 1:
-      dd.increment(`dm-message.edits`)
-      break
+  console.log(`[INFO] Edited ${msg.id}`)
+  const tags = [
+    `channel:${msg.channel.id}`,
+    `user:${msg.author.id}`,
+    `msg:${msg.id}`,
+    `chan-type:${msg.channel.type}`
+  ]
+  if ((msg.channel as GuildChannel).guild !== undefined) {
+    tags.push(`guild:${(msg.channel as GuildChannel).guild.id}`)
   }
+  dd.increment(`message.edits`, 1, tags)
 })
 
 bot.on('messageDelete', msg => {
-  dd.increment(`message.deletes`)
-  dd.increment(`message.deletes_${msg.channel.id}`)
-  if (!msg.channel) {
-    return
+  console.log(`[INFO] Deleted ${msg.id}`)
+  const tags = [
+    `channel:${msg.channel.id}`,
+    `msg:${msg.id}`,
+    `chan-type:${msg.channel.type}`
+  ]
+  if ((msg as Message).author !== undefined) {
+    tags.push(`user:${(msg as Message).author.id}`)
   }
-  switch (msg.channel.type) {
-    case 0:
-      dd.increment(`guild-message.deletes_${(msg.channel as GuildChannel).guild.id}`)
-      break
-    case 1:
-      dd.increment(`dm-message.deletes`)
-      break
+  if ((msg.channel as GuildChannel).guild !== undefined) {
+    tags.push(`guild:${(msg.channel as GuildChannel).guild.id}`)
   }
+  dd.increment(`message.deletes`, 1, tags)
 })
 
-bot.on('guildMemberAdd', guild => {
-  dd.increment(`member.joins_${guild.id}`)
+bot.on('guildMemberAdd', (guild, user) => {
+  console.log(`[INFO] Guild member joined ${guild.id}`)
+  dd.increment(`member.joins`, 1, [
+    `guild:${guild.id}`,
+    `user:${user.id}`
+  ])
 })
 
-bot.on('guildMemberRemove', guild => {
-  dd.increment(`member.removes_${guild.id}`)
+bot.on('guildMemberRemove', (guild, user) => {
+  console.log(`[INFO] Guild member removed from ${guild.id}`)
+  dd.increment(`member.removes`, 1, [
+    `guild:${guild.id}`,
+    `user:${user.id}`
+  ])
 })
 
-bot.on('presenceUpdate', statusUpdate)
+bot.on('presenceUpdate', () => {
+  console.log(`[INFO] Collecting user status.`)
+  statusUpdate()
+})
 
-setInterval(statusUpdate, 5000)
+bot.on('messageReactionAdd', (msg, e, u) => {
+  console.log(`[INFO] Reaction added on ${msg.id}`)
+  const tags = [
+    `channel:${msg.channel.id}`,
+    `user:${u}`,
+    `msg:${msg.id}`,
+    `chan-type:${msg.channel.type}`
+  ]
+  if (e.id === null) {
+    tags.push(`emoji:${emoji.find(e.name).key}`)
+  } else {
+    tags.push(`emoji:${e.name}`)
+    tags.push(`custom-emoji:${e.id}`)
+  }
+  if ((msg.channel as GuildChannel).guild !== undefined) {
+    tags.push(`guild:${(msg.channel as GuildChannel).guild.id}`)
+  }
+  dd.increment(`reactions.add`, 1, tags)
+})
+
+bot.on('messageReactionRemove', (msg, e, u) => {
+  console.log(`[INFO] Reaction removed on ${msg.id}`)
+  const tags = [
+    `channel:${msg.channel.id}`,
+    `user:${u}`,
+    `msg:${msg.id}`,
+    `chan-type:${msg.channel.type}`
+  ]
+  if (e.id === null) {
+    tags.push(`emoji:${emoji.find(e.name).key}`)
+  } else {
+    tags.push(`emoji:${e.name}`)
+    tags.push(`custom-emoji:${e.id}`)
+  }
+  if ((msg.channel as GuildChannel).guild !== undefined) {
+    tags.push(`guild:${(msg.channel as GuildChannel).guild.id}`)
+  }
+  dd.increment(`reactions.remove`, 1, tags)
+})
+
+bot.on('ready', () => {
+  console.log(`[INFO] Connected to discord as ${bot.user.username}#${bot.user.discriminator}`)
+})
+
+process.on('SIGTERM', () => {
+  console.log('[INFO] SIGTERM recieved, disconnecting..')
+  bot.disconnect({
+    reconnect: false
+  })
+  dd.flush()
+})
+
+setInterval(() => {
+  console.log('[INFO] Collecting user status')
+  statusUpdate()
+}, 60000)
 
 bot.connect()
   .catch(console.error)
